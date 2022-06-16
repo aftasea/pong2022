793e248241619e552b968ab38a1d2d0b640d2726
Andres Tovar <andrest@seriously.com>
2022-06-16 15:50:02 +0300

    Refactoring the Ball script to have its functions with single responsibility

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -6,16 +6,15 @@ public class Ball : MonoBehaviour
 {
     public float initialSpeed = 5f;
     public float maxSpeed = 10f;
-    public float speed;
     public float hitAcceleration = 0.1f;
     public Bounds bounds;
     
+    private float speed;
     private float gameFieldTop;
     private float gameFieldBottom;
-    private const float HalfSize = 0.15f / 2f;
+    private const float collisionCorrectionOffset = 0.01f;
 
-    [SerializeField]
-    private Vector3 direction = new Vector3(1f, 0f, 0f);
+    [SerializeField] private Vector3 direction = new Vector3(1f, 0f, 0f);
     
     private Paddle leftPaddle;
     private Paddle rightPaddle;
@@ -24,7 +23,7 @@ public class Ball : MonoBehaviour
     public void Init(float screenHeight, Paddle leftPaddle, Paddle rightPaddle) {
         gameFieldTop = screenHeight;
         gameFieldBottom = -screenHeight;
-        direction = direction.normalized;
+        Vector3.Normalize(direction);
         this.leftPaddle = leftPaddle;
         this.rightPaddle = rightPaddle;
         Reset();
@@ -33,57 +32,6 @@ public class Ball : MonoBehaviour
     public void Reset() {
         transform.position = Vector3.zero;
         speed = initialSpeed;
-    }
-
-    public void UpdatePosition() {
-        if (!canMove)
-            return;
-        
-        float distance = speed * Time.deltaTime;
-        Vector3 currentPos = transform.position;
-        Vector3 targetPos = currentPos + (direction * distance);
-
-        if (targetPos.y > 0f && targetPos.y + HalfSize > gameFieldTop) {
-            targetPos.y = gameFieldTop - HalfSize;
-            direction.y *= -1;
-        }
-        else if (targetPos.y < 0f && targetPos.y - HalfSize < gameFieldBottom) {
-            targetPos.y = gameFieldBottom + HalfSize;
-            direction.y *= -1;
-        }
-        
-        var hit = Physics2D.Raycast(currentPos, direction, distance);
-        if (hit) {
-            Paddle paddle;
-            if (direction.x > 0f) {
-                paddle = rightPaddle;
-                targetPos.x = paddle.transform.position.x - paddle.bounds.extents.x - bounds.extents.x;
-            }
-            else {
-                paddle = this.leftPaddle;
-                targetPos.x = paddle.transform.position.x + paddle.bounds.extents.x + bounds.extents.x;
-            }
-            this.BounceX();
-
-            float distanceFromCenter = targetPos.y - paddle.transform.position.y;
-            float ratio = distanceFromCenter / paddle.bounds.size.y;
-            direction.y = ratio;
-            direction = direction.normalized;
-        }
-        
-        transform.position = targetPos;
-        bounds.center = targetPos;
-    }
-
-    private void BounceX() {
-        direction.x *= -1;
-        speed = Mathf.Min(speed + hitAcceleration, maxSpeed);
-    }
-    
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(bounds.center, bounds.size);
     }
 
     public void Serve() {
@@ -95,5 +43,65 @@ public class Ball : MonoBehaviour
         Reset();
         yield return new WaitForSeconds(1.5f);
         canMove = true;
+    }
+
+    public void UpdatePosition() {
+        if (!canMove)
+            return;
+        
+        float distance = speed * Time.deltaTime;
+        Vector3 currentPos = transform.position;
+        Vector3 targetPos = currentPos + (direction * distance);
+
+        CheckVerticalBordersCollision(ref targetPos);
+        CheckPaddlesCollision(currentPos, ref targetPos, distance);
+
+        transform.position = targetPos;
+        bounds.center = targetPos;
+    }
+
+    private void CheckVerticalBordersCollision(ref Vector3 targetPos) {
+        float ballHalfSize = this.bounds.extents.y;
+        if (targetPos.y > 0f && targetPos.y + ballHalfSize > gameFieldTop) {
+            targetPos.y = gameFieldTop - ballHalfSize;
+            direction.y *= -1;
+        }
+        else if (targetPos.y < 0f && targetPos.y - ballHalfSize < gameFieldBottom) {
+            targetPos.y = gameFieldBottom + ballHalfSize;
+            direction.y *= -1;
+        }
+    }
+
+    private void CheckPaddlesCollision(Vector3 currentPos, ref Vector3 targetPos, float distance) {
+        if (Physics2D.BoxCast(currentPos, bounds.size, 0f, direction, distance)) {
+            Paddle paddle = direction.x < 0f ? leftPaddle : rightPaddle;
+            AdjustHorizontalPositionAfterHit(ref targetPos, paddle);
+            BounceHorizontally();
+            CalculateVerticalDirAfterPaddleHit(targetPos, paddle);
+        }
+    }
+
+    private void AdjustHorizontalPositionAfterHit(ref Vector3 targetPos, Paddle paddle) {
+        float minAllowedDistancePaddleBall = paddle.bounds.extents.x + this.bounds.extents.x + collisionCorrectionOffset;
+        targetPos.x = paddle.transform.position.x + (-this.direction.x * minAllowedDistancePaddleBall);
+    }
+
+    private void BounceHorizontally() {
+        direction.x *= -1;
+        speed = Mathf.Min(speed + hitAcceleration, maxSpeed);
+    }
+
+    private void CalculateVerticalDirAfterPaddleHit(Vector3 targetPos, Paddle paddle) {
+        float distanceFromCenter = targetPos.y - paddle.transform.position.y;
+        float ratio = distanceFromCenter / paddle.bounds.size.y;
+        direction.y = ratio;
+        Vector3.Normalize(direction);
+    }
+    
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
     }
 }
